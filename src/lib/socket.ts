2698ba71e6e17975f6030f890318609f70907ff2
mahinul061 <mahinulislam0611@gmail.com>
2026-08-09 13:49:18 +0600

    socket

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -6,3 +6,12 @@ export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
   withCredentials: true,
 });
+
+socket.on("connect", () => {
+  console.log("✅ CONNECTED");
+  socket.emit("get_online_users");
+});
+
+socket.on("connect_error", (e) => {
+  console.log("❌ FAILED:", e.message);
+});
